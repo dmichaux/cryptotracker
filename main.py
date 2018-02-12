18f172055746894fbcd38a0f03b547a849a031fc
David <michaux.dr@gmail.com>
2018-02-12 15:20:06 -0500

    Add function to scrape data from www.xe.com for currency against 1 USD

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 import requests
+import re
+import sqlite3
+import argparse
 
-print("Running CryptoTracker ...")
+
+def usage():
+	# Display usage and accepted arguments for command-line argv
+	"""
+	"""
+
+
+def main():
+	print("Running CryptoTracker ...")
+	while True:
+		currency_code = input("\nEnter a currency code [0 to exit]:\n").upper()
+		if currency_code == "0":
+			break
+		amount = get_against_usd(currency_code)
+		print(f"1 USD = {amount} {currency_code}")
+
+
+def get_against_usd(currency_code):
+	convert = {"Amount": "1", "From": "USD", "To": currency_code}
+	scraped = requests.get("http://www.xe.com/currencyconverter/convert/", params=convert)
+	match = re.search("uccResultAmount'>(.+?)</span>", scraped.text)
+	amount = match.group(1)
+	return amount
+
+
+if __name__ == '__main__':
+	main()
