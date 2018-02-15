fbb13a42adecf1da60ae90ccaf9a674723499361
David <michaux.dr@gmail.com>
2018-02-15 12:32:50 -0500

    Eliminate duplicate code by simplifying 4 scraping functions into one function with a loop

--- main.py
+++ main.py
@@ -16,10 +16,14 @@ def main():
 	euro_amount = get_against_usd("EUR")
 	yuan_amount = get_against_usd("CNY")
 	print(f"1 USD = {euro_amount} EUR = {yuan_amount} CNY")
-	bitcoin_amount = get_cryptocurrency_avg("BTC")
-	ethereum_amount = get_cryptocurrency_avg("ETH")
-	ripple_amount = get_cryptocurrency_avg("XRP")
-	litecoin_amount = get_cryptocurrency_avg("LTC")
+	bitcoin_average = get_cryptocurrency_avg("BTC")
+	ethereum_average = get_cryptocurrency_avg("ETH")
+	ripple_average = get_cryptocurrency_avg("XRP")
+	litecoin_average = get_cryptocurrency_avg("LTC")
+	print(f"Bitcoin Average: {bitcoin_average}")
+	print(f"Ethereum Average: {ethereum_average}")
+	print(f"Ripple Average: {ripple_average}")
+	print(f"Litecoin Average: {litecoin_average}")
 
 
 def get_against_usd(currency_code):
@@ -31,44 +35,49 @@ def get_against_usd(currency_code):
 
 
 def get_cryptocurrency_avg(currency):
-	amount1 = get_from_coinmarketcap(currency)
-	amount2 = get_from_cryptocoincharts(currency)
-	amount3 = get_from_cryptocurrencychart(currency)
-	amount4 = get_from_bitfinex(currency)
-	print(f"{currency} - 1:{amount1}, 2:{amount2}, 3:{amount3}, 4:{amount4}")
-	# return amount1
+	total = 0
+	sites_regex = {"https://coinmarketcap.com/": '</a></span>[\s\S]+?#markets"[\s\S]+?usd="(.+?)"',
+		"https://cryptocoincharts.info/coins/info": '[\s\S]+?data-usd="(.+?)">',
+		"https://www.cryptocurrencychart.com/": '\)</a>[\s\S]+?data-sort="(.+?)"',
+		"https://www.bitfinex.com/stats": '<span[\s\S]+?USD</td>[\s\S]+?currency">(.+?)</td>'}
+	for site, regex in sites_regex.items():
+		scraped = requests.get(site)
+		match = re.search(currency + regex, scraped.text)
+		total += float(match.group(1))
+	average = round((total / 4), 3)
+	return average
 
 
-def get_from_coinmarketcap(currency):
-	regex = currency + '</a></span>[\s\S]+?#markets"[\s\S]+?usd="(.+?)"'
-	scraped = requests.get("https://coinmarketcap.com/")
-	match = re.search(regex, scraped.text)
-	amount = match.group(1)
-	return amount
+# def get_from_coinmarketcap(currency):
+# 	regex = currency + '</a></span>[\s\S]+?#markets"[\s\S]+?usd="(.+?)"'
+# 	scraped = requests.get("https://coinmarketcap.com/")
+# 	match = re.search(regex, scraped.text)
+# 	amount = match.group(1)
+# 	return amount
 
 
-def get_from_cryptocoincharts(currency):
-	regex = currency + '[\s\S]+?data-usd="(.+?)">'
-	scraped = requests.get("https://cryptocoincharts.info/coins/info")
-	match = re.search(regex, scraped.text)
-	amount = match.group(1)
-	return amount
+# def get_from_cryptocoincharts(currency):
+# 	regex = currency + '[\s\S]+?data-usd="(.+?)">'
+# 	scraped = requests.get("https://cryptocoincharts.info/coins/info")
+# 	match = re.search(regex, scraped.text)
+# 	amount = match.group(1)
+# 	return amount
 
 
-def get_from_cryptocurrencychart(currency):
-	regex = currency + '\)</a>[\s\S]+?data-sort="(.+?)"'
-	scraped = requests.get("https://www.cryptocurrencychart.com/")
-	match = re.search(regex, scraped.text)
-	amount = match.group(1)
-	return amount
+# def get_from_cryptocurrencychart(currency):
+# 	regex = currency + '\)</a>[\s\S]+?data-sort="(.+?)"'
+# 	scraped = requests.get("https://www.cryptocurrencychart.com/")
+# 	match = re.search(regex, scraped.text)
+# 	amount = match.group(1)
+# 	return amount
 
 
-def get_from_bitfinex(currency):
-	regex = currency + '<span[\s\S]+?USD</td>[\s\S]+?currency">(.+?)</td>'
-	scraped = requests.get("https://www.bitfinex.com/stats")
-	match = re.search(regex, scraped.text)
-	amount = match.group(1)
-	return amount
+# def get_from_bitfinex(currency):
+# 	regex = currency + '<span[\s\S]+?USD</td>[\s\S]+?currency">(.+?)</td>'
+# 	scraped = requests.get("https://www.bitfinex.com/stats")
+# 	match = re.search(regex, scraped.text)
+# 	amount = match.group(1)
+# 	return amount
 
 
 if __name__ == '__main__':
